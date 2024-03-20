9addbf9232482ea6330928749ba46b5e796c1658
yonezu60 <zzz@4F0qM.com>
2024-03-20 21:53:45 +0800

    1

---
+++ precache-manifest.119e7115bb50c9bff4673441abc350bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afda6c33e250abacb96d6f6524db2baa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dad1a3e38249d8ad1c8",
+    "url": "/static/js/2.152860f4.chunk.js"
+  },
+  {
+    "revision": "d255601c39ba9ea8be3f576f2fb7e1d5",
+    "url": "/static/js/2.152860f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b3f7e626a18b09c21e5",
+    "url": "/static/js/main.4a7587dc.chunk.js"
+  },
+  {
+    "revision": "c7805d0a662790c235f8",
+    "url": "/static/js/runtime-main.c0105f97.js"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "/static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "/static/media/logo.5827780d.svg"
+  },
+  {
+    "revision": "edb44e56409a7af705c227bd272fac47",
+    "url": "/static/media/logo_white.edb44e56.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
